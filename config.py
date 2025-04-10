cc0bd122fc5669a74a5b3ce653ac655e282c000f
bhanuchetia <dramawave7@gmail.com>
2025-04-10 12:12:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     DATABASE_URI = os.environ.get('DATABASE_URI', "")
-    DATABASE_NAME = os.environ.get('DATABASE_NAME', "telebot")
+    DATABASE_NAME = os.environ.get('DATABASE_NAME', "Cluster0")
